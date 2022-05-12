92cff279dc293709135f2c12efe8ba0268249901
ha-yun <deu05230@naver.com>
2022-05-12 16:26:17 +0900

    profile수정

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $(function(){
     var pro_show = $("#project_show");
     var BtnList = $('.btn_list').children('ul');
-    var profile = $("#profile");
+    var footer = $("footer");
     var Project_li = $("#project").children('ul').children('li');
 
     ShowList();
     Event();
     function ShowList(){
         let SCROLL= $(window).scrollTop();
-        if(pro_show.offset().top < SCROLL && SCROLL < profile.offset().top ){
+        if(pro_show.offset().top < SCROLL && SCROLL < footer.offset().top ){
             BtnList.fadeIn();
             $(Project_li).each(function(i){
                 if(SCROLL > Project_li.eq(i).offset().top - 80){
