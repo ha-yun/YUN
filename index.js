0ddcc34b515179eefb18b36ed33221c4ce498a43
ha-yun <deu05230@naver.com>
2022-06-14 19:29:06 +0900

    skill_list and color수정

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 $(function(){
+    var skills_list1 = $("#skills_list1");
+    var skills_list2 = $("#skills_list2");
+    var skills_list1_btn = skills_list1.children("button");
+    var skills_list2_btn = skills_list2.children("button");
+
+    function SkillsLeft(){
+        let skills_right = $("#skills_right");
+        if(skills_right.css("opacity") == '0'){
+            skills_list2.animate({opacity : "0"},400);
+            skills_right.animate({opacity: "1"}, 50);
+        }else{
+            skills_list2.animate({opacity : "1"},400);
+            skills_right.animate({opacity: "0"}, 50);
+        }
+    }
+    function SkillsRight(){
+        let skills_left = $("#skills_left");
+        if(skills_left.css("opacity") == '0'){
+            skills_list1.animate({opacity : "0"},400);
+            skills_left.animate({opacity: "1"}, 50);
+        }else{
+            skills_list1.animate({opacity : "1"},400);
+            skills_left.animate({opacity: "0"}, 50);
+        }
+    }
+
+
     var pro_show = $("#project_show");
     var BtnList = $('.btn_list').children('ul');
     var footer = $("footer");
@@ -24,5 +51,7 @@ $(function(){
 
     function Event(){
         $(window).on('scroll',ShowList);
+        skills_list1_btn.on('click', SkillsLeft);
+        skills_list2_btn.on('click', SkillsRight);
     }
 })
